f868194aecb2a22e08c7d4db45a8bb642c1bf249
PkPawarat <popawarat@gmail.com>
2023-10-06 19:41:34 +1100

    Pk add

--- main.m
+++ main.m
@@ -11,26 +11,28 @@ classdef main
     
     methods
         function self = main()
-            
+            clf;
+            self.setupEnvironment();
+            self = self.SetupRobots();
         end
-        
-        function SetupEnvironment(self)
 
-        end
-        function SetupRobots(self)
-            self.robot1 = LinearUR5;
-            self.robot2 = babyYODA;
+        function self = SetupRobots(self)
+            self.robot1 = LinearUR5(transl(-0.4,0,0.5));
+            self.robot2 = babyYODA(transl(-0.4,1,0.5));
 
             % set the location according table
 
         end
         function AssignTarget(self)
+
         end
             
-        function setupEnvironment()
-            u = PlaceObject('table_v1.ply', [-0.4,0,0]);
-            u = PlaceObject('table_v1.ply', [-0.4,2,0]);
-            robot = LinearUR5(transl(0,0,0.5));
+        function setupEnvironment(self)
+            u1 = PlaceObject('table_v1.ply', [-0.4,0,0]);
+            hold on;
+            u2 = PlaceObject('table_v1.ply', [-0.4,1,0]);
+            hold on;
+            % robot = LinearUR5(transl(0,0,0.5));
             %second robot
         end
         
