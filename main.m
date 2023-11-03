6075d0f016828253bcf6eb7a60475f2e393657b0
PkPawarat <popawarat@gmail.com>
2023-11-03 16:06:47 +1100

    Update line intersection

--- main.m
+++ main.m
@@ -84,7 +84,7 @@ classdef main
             self.pose = cell(1,2);
             self.pose{1} = self.robots{1}.model.fkine(self.robots{1}.homeQ).T;
             self.pose{2} = self.robots{2}.model.fkine(self.robots{2}.homeQ).T;
-
+            
             % self.DisplayAllPossiblePositionAndWorkspace('logs file/FanucM20.txt');
             % self.DisplayAllPossiblePositionAndWorkspace('logs file/PositionUR5CanDo.txt');
             % 
@@ -139,15 +139,35 @@ classdef main
             PlaceObject('model3D/Star_wars_JAWA.ply', [0,5,0]);
             PlaceObject('model3D/Star_wars_JAWA.ply', [1,5,0]);
             PlaceObject('model3D/Star_wars_JAWA.ply', [2,5,0]);
-            PlaceObject('model3D/TABLER~1.PLY', [0,4,0]);
-            PlaceObject('model3D/fireExtinguisher.ply', [1,4,0]);
-            PlaceObject('model3D/emergencyStopButton.ply', [-0.2,4,0.25]);
+            PlaceObject('model3D/TABLER~1.PLY', [0,4.5,0]);
+            PlaceObject('model3D/fireExtinguisher.ply', [1,4.5,0]);
+            PlaceObject('model3D/emergencyStopButton.ply', [-0.2,4.5,0.25]);
             PlaceObject('model3D/Traffic Cone.ply', [-2,3.5,0]);
             PlaceObject('model3D/Traffic Cone.ply', [2,3.5,0]);
             PlaceObject('model3D/Traffic Cone.ply', [-2,-2,0]);
             PlaceObject('model3D/Traffic Cone.ply', [2,-2,0]);
 
             view(3);
+
+            % Parameters for the box
+            box_center = [0, 1, 0]; % The center of the box
+            box_width = 4; % Width of the box
+            box_height = 6; % Height of the box
+            box_depth = 4; % Depth of the box
+            
+            % Create the box
+            box_planes = self.createBox(box_center, box_width, box_height, box_depth);
+            
+            % Plot the planes of the box
+            colors = ['r', 'g', 'b', 'y']; % Different color for each plane
+            for i = 1:4
+                surf(box_planes(i).x_grid, box_planes(i).y_grid, box_planes(i).z_grid, ...
+                     'FaceColor', colors(i), 'FaceAlpha', 0.5);
+                hold on;
+            end
+
+
+
             axis([-3.5 5 -3.5 6 -0.1 4]);
             % axis([-3 3 -3 3 0.1 4]);
             camlight;
@@ -882,6 +902,144 @@ classdef main
                 pause(0.001);
             end 
         end
+
+        function [x_grid, y_grid, z_grid] = createPlane(self,point, width, height)
+            % point: A 1x3 vector [x, y, z] representing a point on the plane
+            % width: The width of the plane along the X-axis
+            % height: The height of the plane along the Y-axis
+            
+            % Create a grid of points for the plane
+            [y_grid, z_grid] = meshgrid(linspace(-height/2, height/2, 10), linspace(-width/2, width/2, 10));
+            x_grid = point(1) * ones(size(y_grid)); % x is constant for a vertical plane
+            
+            % Adjust y and z to be centered around the provided point
+            y_grid = y_grid + point(2);
+            z_grid = z_grid + point(3);
+        end
+        function intersect_point = lineIntersection(self,obj_start, obj_direction, plane_x)
+            % obj_start: A 1x3 vector [x, y, z] representing the starting point of the line
+            % obj_direction: A 1x3 vector [dx, dy, dz] representing the direction of the line
+            % plane_x: The x-coordinate of the vertical plane
+            
+            % Calculate the parameter t at which the line intersects the plane
+            t_intersect = (plane_x - obj_start(1)) / obj_direction(1);
+            
+            % Calculate the intersection point
+            intersect_point = obj_start + t_intersect * obj_direction;
+        end
+        % function [planes] = createBox(self,center, width, height, depth)
+        %     % center: A 1x3 vector [x, y, z] representing the center of the box
+        %     % width: The width of the box along the X-axis
+        %     % height: The height of the box along the Y-axis
+        %     % depth: The depth of the box along the Z-axis
+        %     hold on;
+        %     % Initialize the planes structure
+        %     planes = struct('x_grid', [], 'y_grid', [], 'z_grid', []);
+        % 
+        %     % Half-dimensions
+        %     halfWidth = width / 2;
+        %     halfHeight = height / 2;
+        %     halfDepth = depth / 2;
+        % 
+        %     % Define the grid for each plane
+        %     [y_grid, z_grid] = meshgrid(linspace(-halfHeight, halfHeight, 10), linspace(-halfDepth, halfDepth, 10));
+        % 
+        %     % Front plane (x is constant)
+        %     planes(1).x_grid = (center(1) + halfWidth) * ones(size(y_grid));
+        %     planes(1).y_grid = y_grid + center(2);
+        %     planes(1).z_grid = z_grid + center(3);
+        % 
+        %     % surf(planes(1).x_grid, planes(1).y_grid, planes(1).z_grid, ...
+        %     %      'FaceColor', 'r', 'FaceAlpha', 0.5);
+        % 
+        %     % Back plane (x is constant)
+        %     planes(2).x_grid = (center(1) - halfWidth) * ones(size(y_grid));
+        %     planes(2).y_grid = y_grid + center(2);
+        %     planes(2).z_grid = z_grid + center(3);
+        % 
+        %     % surf(planes(2).x_grid, planes(2).y_grid, planes(2).z_grid, ...
+        %     %      'FaceColor', 'r', 'FaceAlpha', 0.5);
+        % 
+        %     % Left plane (y is constant)
+        %     planes(3).y_grid = (center(2) - halfHeight) * ones(size(z_grid));
+        %     planes(3).x_grid = z_grid + center(1); % z_grid reused for x since y is constant
+        %     planes(3).z_grid = y_grid + center(3); % y_grid reused for z
+        % 
+        %     % surf(planes(3).x_grid, planes(3).y_grid, planes(3).z_grid, ...
+        %     %      'FaceColor', 'r', 'FaceAlpha', 0.5);
+        % 
+        %     % Right plane (y is constant)
+        %     planes(4).y_grid = (center(2) + halfHeight) * ones(size(z_grid));
+        %     planes(4).x_grid = z_grid + center(1); % z_grid reused for x since y is constant
+        %     planes(4).z_grid = y_grid + center(3); % y_grid reused for z
+        % 
+        %     % surf(planes(4).x_grid, planes(4).y_grid, planes(4).z_grid, ...
+        %     %      'FaceColor', 'r', 'FaceAlpha', 0.5);
+        % 
+        % end
+        function [planes] = createBox(self, center, width, height, depth)
+            % center: A 1x3 vector [x, y, z] representing the center of the box
+            % width: The width of the box along the X-axis
+            % height: The height of the box along the Y-axis
+            % depth: The depth of the box along the Z-axis
+            
+            % Initialize the planes array
+            planes = struct('x_grid', {}, 'y_grid', {}, 'z_grid', {});
+            
+            % Half-dimensions
+            halfWidth = width / 2;
+            halfHeight = height / 2;
+            halfDepth = depth / 2;
+            
+            % Define the grid for each plane
+            [y_grid, z_grid] = meshgrid(linspace(-halfHeight, halfHeight, 10), linspace(-halfDepth, halfDepth, 10));
+            [x_grid, z_grid_x] = meshgrid(linspace(-halfWidth, halfWidth, 10), linspace(-halfDepth, halfDepth, 10));
+            
+            % Front plane (x is constant)
+            planes(end+1) = struct('x_grid', (center(1) + halfWidth) * ones(size(y_grid)), ...
+                                    'y_grid', y_grid + center(2), ...
+                                    'z_grid', z_grid + center(3));
+            
+            % Back plane (x is constant)
+            planes(end+1) = struct('x_grid', (center(1) - halfWidth) * ones(size(y_grid)), ...
+                                    'y_grid', y_grid + center(2), ...
+                                    'z_grid', z_grid + center(3));
+            
+            % Left plane (y is constant)
+            planes(end+1) = struct('x_grid', x_grid + center(1), ...
+                                    'y_grid', (center(2) - halfHeight) * ones(size(x_grid)), ...
+                                    'z_grid', z_grid_x + center(3));
+            
+            % Right plane (y is constant)
+            planes(end+1) = struct('x_grid', x_grid + center(1), ...
+                                    'y_grid', (center(2) + halfHeight) * ones(size(x_grid)), ...
+                                    'z_grid', z_grid_x + center(3));
+            
+            % Top plane (z is constant)
+            planes(end+1) = struct('x_grid', x_grid + center(1), ...
+                                    'y_grid', y_grid + center(2), ...
+                                    'z_grid', (center(3) + halfDepth) * ones(size(x_grid)));
+            
+            % Bottom plane (z is constant)
+            planes(end+1) = struct('x_grid', x_grid + center(1), ...
+                                    'y_grid', y_grid + center(2), ...
+                                    'z_grid', (center(3) - halfDepth) * ones(size(x_grid)));
+        end
+
+        function isInside = checkInsideBox(self,point, box_center, width, height, depth)
+            % Check if a point is inside the given box
+            halfWidth = width / 2;
+            halfHeight = height / 2;
+            halfDepth = depth / 2;
+            
+            isInside = point(1) >= (box_center(1) - halfWidth) && ...
+                       point(1) <= (box_center(1) + halfWidth) && ...
+                       point(2) >= (box_center(2) - halfHeight) && ...
+                       point(2) <= (box_center(2) + halfHeight) && ...
+                       point(3) >= (box_center(3) - halfDepth) && ...
+                       point(3) <= (box_center(3) + halfDepth);
+        end
+
 
 
     end
